04d8c8504ccee8ffef6920eb99ece37d4035b69b
afazio1 <alexa.fazio04@gmail.com>
2024-04-11 02:34:32 -0400

    add set cookie

--- frontend/src/screens/Login.tsx
+++ frontend/src/screens/Login.tsx
@@ -1,9 +1,28 @@
 import { Link } from "react-router-dom"
 import "../styles/Login.css"
+import { FormEvent } from "react";
+
 export default function Login() {
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+        const formData = new FormData(e.target as HTMLFormElement);
+        const username = formData.get('username');
+        const password = formData.get('password');
+        fetch('http://localhost:8000/api/login/', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        }
     return (
         <main>
-            <form className="login-container">
+            <form className="login-container" onSubmit={handleSubmit}>
                 <h1>Login</h1>
                 <div className="form-input">
                     <label htmlFor="username">Username</label>
--- frontend/src/screens/Signup.tsx
+++ frontend/src/screens/Signup.tsx
@@ -1,10 +1,36 @@
 import { Link } from "react-router-dom";
 import "../styles/Login.css"
+import { FormEvent } from "react";
 export default function Signup() {
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+        const formData = new FormData(e.target as HTMLFormElement);
+        const email = formData.get('email');
+        const username = formData.get('username');
+        const password = formData.get('password');
+        fetch('http://localhost:8000/api/signup/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, username, password }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    }
     return (
         <main>
-            <form className="login-container">
+            <form className="login-container" onSubmit={handleSubmit}>
                 <h1>Sign Up</h1>
+                <div className="form-input">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" />
+                </div>
                 <div className="form-input">
                     <label htmlFor="username">Username</label>
                     <input type="text" name="username" id="username" />
