443bc2e13e8eaeaa713e12547b3c1e2b21d87416
Sahej Panag <spanag3@gatech.edu>
2024-04-12 15:23:40 -0400

    second screen draft

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,21 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainSearch from './components/MainSearch';
+import Results from './components/Results';
 
 function App() {
-
   return (
-    <>
-      <div className="App" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' , gap: 15}}>
-      <img
-        src="/PeerNotes.png" 
-        alt="PeerNotes Logo" 
-        style={{ 
-          height: "100px"
-        }} 
-      />
-     <MainSearch />
-    </div>
-    </>
-  )
+    <BrowserRouter>
+      <div className="App">
+        <header style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100px', gap: 15 }}>
+        </header>
+        <Routes>
+          <Route path="/" element={<MainSearch />} />
+          <Route path="/results" element={<Results />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App
