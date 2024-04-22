7467092e0f318309944986db611fe44c286c4e4f
afazio1 <alexa.fazio04@gmail.com>
2024-04-22 01:39:25 -0400

    add polling

---
+++ frontend/src/hooks/usePoll.ts
@@ -0,0 +1,33 @@
+import { useEffect } from "react"
+import { getAuthHeaders } from "../utils/getAuthHeaders"
+export const usePoll = () => {
+    useEffect(() => {
+      const interval = setInterval(() => {
+        pollServer()
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+      }, 30 * 60000) // poll every 30 minutes
+      return () => {
+        // clean up
+        clearInterval(interval);
+      };
+    }, [])
+
+    const pollServer = async () => {
+        // request private ip from local api
+      return fetch('http://127.0.0.1:8080/ip')
+        .then(response => response.text())
+        .then((data) => {
+          // send private ip to central server
+          return fetch('http://localhost:8000/api/poll/', {
+            method: 'POST',
+            headers: getAuthHeaders(),
+            body: JSON.stringify({ip: data}),
+          })
+        }).then(response => response.json())
+        .then(data => {
+          console.log("Poll response data:", data);
+        })
+    }
+}
